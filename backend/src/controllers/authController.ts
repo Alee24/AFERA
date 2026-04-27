76d286cacbd930160c822246f06148d54245d33c
Alee24 <mettoalex@gmail.com>
2026-04-28 00:45:25 +0300

    Fix missing Staff import in authController (resolved 500 login error)

--- backend/src/controllers/authController.ts
+++ backend/src/controllers/authController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { User, Student, Program, Enrollment } from '../models';
+import { User, Student, Staff, Program, Enrollment } from '../models';
 import { sendApplicationNotification } from '../services/mailService';
 
 export const register = async (req: Request, res: Response) => {
