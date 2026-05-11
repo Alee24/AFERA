d302ce85b03b29efaf65dccc1ee002ceb5e04043
Alee24 <mettoalex@gmail.com>
2026-05-12 00:09:34 +0300

    Course: Fix build error due to missing useMemo import

--- frontend/src/app/[lang]/courses/[id]/page.tsx
+++ frontend/src/app/[lang]/courses/[id]/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { 
   Clock, 
